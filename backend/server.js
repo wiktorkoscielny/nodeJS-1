9f6c8e513f5f3f54930567ec5206dcc22d6d9d91
Wiktor <wiktorkoscielny@gmail.com>
2022-10-05 11:54:48 +0200

    I created functions that handle the functionalities of the 'todo' application both on the frontend and backend side

--- backend/server.js
+++ backend/server.js
@@ -8,21 +8,66 @@ app.use(cors());
 // middleware
 app.use(express.json())
 
+// todo storage
 const todoList = [];
 
 app.get("/", (req, res) => {
-  res.send("Server is working!");
+    res.send("Server is working!");
 });
+
+// add new todo
 app.post("/todos", (req, res) => {
-//   console.log(req.body);
-  todoList.push(req.body)
-  res.status(200).end();
+    //   console.log(req.body);
+    todoList.push(req.body)
+    res.status(200).end();
 });
+
+// fetching todos on component render/update
 app.get('/todos', (req, res) => {
-    res.json({todoList})
+    res.json({ todoList });
 })
 
+// deleting todos
+app.delete("/todos/:todoId", (req, res) => {
+    const todoId = req.params.todoId;
+    const todoItemIndex = todoList.findIndex((e) => e.id === todoId);
+
+    if (todoItemIndex !== undefined) {
+        todoList.splice(todoItemIndex, 1)
+    } else {
+        res.status(404).end();
+    }
+
+    res.status(200).end();
+})
+
+// delete all todos
+app.delete("/todos", (req, res) => {
+    todoList.splice(0, todoList.length);
+    res.status(200).end();
+})
+
+// update todos
+app.patch("/todos/:todoId", (req, res) => {
+    const todoId = req.params.todoId;
+
+    const todoItem = todoList.find((e) => e.id === todoId)
+
+    if (todoItem !== undefined) {
+        const update = req.body;
+        if (update.check !== undefined) {
+            todoItem.check = update.check;
+        }
+        if (update.body !== undefined) {
+            todoItem.body = update.body;
+        }
+        res.status(200).end();
+    } else {
+        res.status(404).end();
+    }
+    // here i am updating only one task... maybe i should return whole new todolist with updated task ?
+})
 
 app.listen(url, () => {
-  console.log(`Server started on port: ${url}`);
+    console.log(`🚀 Server started on port: ${url}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,38 +12,55 @@ import { useState, useEffect } from "react";
 
 function App() {
   const toast = useToast();
-  
-  // const [tasks, setTasks] = useState(
-  //   () => JSON.parse(localStorage.getItem("tasks")) || []
-  // );
-  const [tasks, setTasks] = useState([])
-  // useEffect(() => {
-  //   localStorage.setItem("tasks", JSON.stringify(tasks));
-  // }, [tasks]);
+  const [tasks, setTasks] = useState([]);
 
+  useEffect(() => {
+    getTodosFromServer();
+  }, []);
 
+  // backend functions
   const getTodosFromServer = async () => {
     try {
       const response = await fetch("http://localhost:5000/todos");
       const data = await response.json();
-      setTasks(data.todoList)
-    } catch(e) {
-      console.log(e)
+      setTasks(data.todoList);
+    } catch (e) {
+      console.log(e);
     }
+  };
+  function deleteItemFromBackend(id) {
+    return fetch(`http://localhost:5000/todos/${id}`, {
+      method: "DELETE",
+    });
   }
 
-  useEffect(() => {
-    getTodosFromServer()
-  }, [tasks])
+  function patchItemToBackEnd(update) {
+    const id = update.id;
+    return fetch(`http://localhost:5000/todos/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify(update),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+  }
+
+  function deleteAllTodosFromBackend() {
+    return fetch("http://localhost:5000/todos", {
+      method: "DELETE",
+    });
+  }
 
   function deleteTask(id) {
     const newTasks = tasks.filter((task) => {
       return task.id !== id;
     });
+    deleteItemFromBackend(id);
     setTasks(newTasks);
   }
 
   function deleteTaskAll() {
+    deleteAllTodosFromBackend();
     setTasks([]);
   }
 
@@ -51,9 +68,13 @@ function App() {
     const newTasksCheck = tasks.map((task, index, array) => {
       if (task.id === id) {
         task.check = !task.check;
+        if (task !== undefined) {
+          patchItemToBackEnd(task);
+        }
       }
       return task;
     });
+
     setTasks(newTasksCheck);
   }
 
@@ -76,6 +97,9 @@ function App() {
       if (task.id === id) {
         task.body = body;
         task.check = false;
+        if (task !== undefined) {
+          patchItemToBackEnd(task);
+        }
       }
       return task;
     });
@@ -92,22 +116,22 @@ function App() {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <VStack p={4} minH='100vh' pb={28}>
+    <VStack p={4} minH="100vh" pb={28}>
       <IconButton
         icon={colorMode === "light" ? <FaSun /> : <FaMoon />}
-        isRound='true'
-        size='md'
-        alignSelf='flex-end'
+        isRound="true"
+        size="md"
+        alignSelf="flex-end"
         onClick={toggleColorMode}
-         aria-label='toogle-dark-mode'
+        aria-label="toogle-dark-mode"
       />
 
       <Heading
-        p='5'
-        fontWeight='extrabold'
-        size='xl'
-        bgGradient='linear(to-r, red.500, yellow.500)'
-        bgClip='text'
+        p="5"
+        fontWeight="extrabold"
+        size="xl"
+        bgGradient="linear(to-r, red.500, yellow.500)"
+        bgClip="text"
       >
         Todo list
       </Heading>
